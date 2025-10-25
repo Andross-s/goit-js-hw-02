d1e81c9ed7516c161d3153bc28d873914f700421
Andrij Skoropad <andross_s@ukr.net>
2025-10-25 16:44:18 +0300

    Fin home task

--- js/task-1.js
+++ js/task-1.js
@@ -1,50 +1,14 @@
-// function makeTransaction(quantity, pricePerDroid) {
-//   const totalPrice = quantity * pricePerDroid;
-//   return `You ordered ${quantity} droids worth ${totalPrice} credits!`;
-// }
-
-// console.log(makeTransaction(5, 3000)); // "You ordered 5 droids worth 15000 credits!"
-// console.log(makeTransaction(3, 1000)); // "You ordered 3 droids worth 3000 credits!"
-// console.log(makeTransaction(10, 500)); // "You ordered 10 droids worth 5000 credits!"
-
-// function checkStorage(available, ordered) {
-//   if (available >= ordered) {
-//     return 'Order is processed, our manager will contact you';
-//   } else {
-//     return 'Not enough goods in stock!';
-//   }
-// }
-// // checkStorage(100, 50);
-// console.log('🚀 ~ checkStorage(100, 50):', checkStorage(100, 50));
-// // checkStorage(100, 130);
-// console.log('🚀 ~ checkStorage(100, 130):', checkStorage(100, 130));
-// // checkStorage(200, 20);
-// console.log('🚀 ~ checkStorage(200, 20):', checkStorage(200, 20));
-// // checkStorage(200, 150);
-// console.log('🚀 ~ checkStorage(200, 150):', checkStorage(200, 150));
-// // checkStorage(150, 180);
-// console.log('🚀 ~ checkStorage(150, 180):', checkStorage(150, 180));
-
-function checkStorage(available, ordered) {
-  if (ordered === 0) {
-    return 'There are no products in the order!';
-  } else if (available < ordered) {
-    return 'Your order is too large, there are not enough items in stock!';
+function makeTransaction(quantity, pricePerDroid, customerCredits) {
+  const totalPrice = quantity * pricePerDroid;
+  if (totalPrice > customerCredits) {
+    return 'Insufficient funds!';
   } else {
-    return 'The order is accepted, our manager will contact you';
+    return `You ordered ${quantity} droids worth ${totalPrice} credits!`;
   }
 }
-// checkStorage(100, 50);
-console.log('🚀 ~ checkStorage(100, 50):', checkStorage(100, 50));
-// checkStorage(100, 130);
-console.log('🚀 ~ checkStorage(100, 130):', checkStorage(100, 130));
-// checkStorage(70, 0);
-console.log('🚀 ~ checkStorage(70, 0):', checkStorage(70, 0));
-// checkStorage(200, 20);
-console.log('🚀 ~ checkStorage(200, 20):', checkStorage(200, 20));
-// checkStorage(200, 250);
-console.log('🚀 ~ checkStorage(200, 250):', checkStorage(200, 250));
-// checkStorage(150, 0);
-console.log('🚀 ~ checkStorage(150, 0):', checkStorage(150, 0));
-// checkStorage(80, 80);
-console.log('🚀 ~ checkStorage(80, 80):', checkStorage(80, 80));
+
+console.log(makeTransaction(5, 3000, 23000)); // "You ordered 5 droids worth 15000 credits!"
+console.log(makeTransaction(3, 1000, 15000)); // "You ordered 3 droids worth 3000 credits!"
+console.log(makeTransaction(10, 5000, 8000)); // "Insufficient funds!"
+console.log(makeTransaction(8, 2000, 10000)); // "Insufficient funds!"
+console.log(makeTransaction(10, 500, 5000)); // "You ordered 10 droids worth 5000 credits!"
